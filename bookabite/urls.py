7d8f3f78421a4802a5bf791ee632510ab682904c
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-06-16 18:17:27 -0300

    feat: setting up django-admin-toolbar + templates + views

--- bookabite/urls.py
+++ bookabite/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# URL Configuration for BookABite app
+# URLConf for BookABite app
 urlpatterns = [
-    path("home/", views.index, name="index"),
+    path('home/', views.index, name="index"),
 ]
--- restaurante_back/urls.py
+++ restaurante_back/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# URLConf for BookABite root app
 from django.contrib import admin
 from django.urls import include, path
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("bookabite/", include("bookabite.urls")),
+    path('bookabite/', include('bookabite.urls')),
+    path('__debug__/', include('debug_toolbar.urls'))
 ]
