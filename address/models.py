2097ebee10b8c12419b077e5d87fa9ca6b0e68c1
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-06-19 18:16:28 -0300

    feat: building entities and models

--- address/models.py
+++ address/models.py
@@ -6,6 +6,6 @@ class Client(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
-    birthdate = models.DateField()
+    birthdate = models.DateField(blank=True, null=False)
     def __str__(self):
         return self.name
--- restaurant/models.py
+++ restaurant/models.py
@@ -3,17 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Restaurant(models.Model):
+    cnpj = models.CharField(max_length=14)
+    name = models.CharField(max_length=100)
     cep = models.CharField(max_length=9)
     street = models.CharField(max_length=30)
     number = models.CharField(max_length=10)
     city = models.CharField(max_length=30)
     state = models.CharField(max_length=2)
     neighborhood = models.CharField(max_length=30)
-    cnpj = models.CharField(max_length=14)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    website = models.URLField()
-    description = models.TextField()
+    website = models.URLField(blank=True, null=False)
+    description = models.TextField(blank=True, null=False)
     image = models.ImageField(upload_to='restaurant/images')
     def __str__(self):
         return self.name
