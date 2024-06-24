0d8bdb50e74404d2de0b7cc6d7975ab64da54152
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-06-24 00:14:44 -0300

    ci: new migration and restaurant_client class name updated

--- restaurant_client/models.py
+++ restaurant_client/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Client(models.Model):
+class RestaurantClient(models.Model):
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     country_code = models.CharField(max_length=3)
     phone = models.CharField(max_length=20, primary_key=True)
-    """ email = models.EmailField() """
-    """ birthdate = models.DateField(blank=True, null=False) """
+    # email = models.EmailField()
+    # birthdate = models.DateField(blank=True, null=False)
     def __str__(self):
         return self.name
