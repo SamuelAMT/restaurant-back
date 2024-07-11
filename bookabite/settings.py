9ece404b657cde510a8b3368b524fd34528302d3
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-07-10 23:12:21 -0300

    ci: attempt to connect to postgresql

--- bookabite/settings.py
+++ bookabite/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'bookabite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
+        'NAME': os.getenv('DB_NAME_DEV'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
