374ad283a01564e527a1415f5815906dda55d3ad
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-07-20 23:57:51 -0300

    feat: setting up endpoints

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,10 @@ from .reservation import router as reservation_router
 
 api = NinjaAPI()
 
+@api.get("/hello")
+def hello(request):
+    return {"message": "Hello world"}
+
 api.add_router('/restaurant/', restaurant_router)
 api.add_router('/reservation/', reservation_router)
 #api.add_router('/restaurant_customer/', restaurant_customer_router)
