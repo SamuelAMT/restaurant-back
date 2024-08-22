d2ef300381c88efb35db1b7a8e6b4f649430c8ea
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-08-21 23:48:12 -0300

    refac: views.py comments removed

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 # Integrate frontend with backend
 def index(request):
     return render(request, 'reservation/index.html')
