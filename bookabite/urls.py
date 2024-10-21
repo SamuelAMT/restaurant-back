e036753d80188b14cb03b22e177e258e6a89c250
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-10-20 23:57:12 -0300

    fix: bookabite urls.py

--- bookabite/urls.py
+++ bookabite/urls.py
@@ -2,18 +2,17 @@ from django.contrib import admin
 from django.urls import include, path
 import debug_toolbar
 from ninja import NinjaAPI
-from api.custom_auth import auth_router
+from ninja.openapi.urls import get_openapi_urls
 
 app_name = "bookabite"
 
 api = NinjaAPI()
 
-api.add_router("/auth/", auth_router)
+openapi_urls = get_openapi_urls(api)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.v1.urls", namespace="api-v1")),  # API Versioning
     path("__debug__/", include("debug_toolbar.urls")),
-    path("api/openapi/", api.openapi),
-    path("api/swagger/", api.swagger_ui),
+    path("api/openapi/", include(openapi_urls)),
 ]
