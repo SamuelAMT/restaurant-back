08f7d95c2e4bfb485eadc4b25f542f62cbc407cb
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-12-15 23:25:39 -0300

    conf: environment variables

--- bookabite/settings.py
+++ bookabite/settings.py
@@ -248,7 +248,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
